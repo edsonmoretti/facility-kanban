09ff4f44c45543ece7ab43c010fd52a54812d55e
Edson Moretti <edsonmoretti@live.com>
2025-10-30 22:52:10 -0300

    Initialize Spring Boot project with Maven setup and basic configuration

---
+++ src/main/java/br/com/edsonmoretti/facility_kanban/FacilityKanbanApplication.java
@@ -0,0 +1,13 @@
+package br.com.edsonmoretti.facility_kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilityKanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilityKanbanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/edsonmoretti/facility_kanban/FacilityKanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edsonmoretti.facility_kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilityKanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
